cf8bce4d7ab8f4d1da491ccb81191d5024323be9
Maxwell <hernangm2433@student.laccd.edu>
2026-01-26 15:01:34 -0800

    Updated main.cpp and README.md
    - Added security questions
    - Added function to check if something is a number and prevent errors
    - Updated Switch function

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 Allows the user to create an account with a username and password. The user can also login using their username and password.
 
 Features to be added:
-- Security Questions
 - Password and Security Question hashing
 - Forget password option that allows the user to find their password by using their username and answering the security question (Will be changed to allow user to change their password in the future)
--- main.cpp
+++ main.cpp
@@ -1,59 +1,78 @@
 #include <iostream>
-#include <cctype>
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <limits>
+
+// File to be used
+std::string fileName = "Projects/Login/data.txt";
 
 // Declare functions
 void registration();
 void login();
+int isNumber(int min, int max);
 
 int main() {
-    int option;
-    
     // While loop to ask if user wants to register or login
     while (true){
-        std::cout << "Welcome! Would you like to \n(1) Register \n(2) Login \n(3) Exit \nEnter choice:";
-        std::cin >> option;
-        
-        // Error handling for input that isn't a number
-        if (!std::cin) {
-            // Clear fail state
-            std::cin.clear(); 
-            
-            // Clear input
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cout << "Invalid input! Please enter a number.\n\n";
-            continue;
-        }
-        
-        switch(option){
+        std::cout << "Welcome! Would you like to \n(1) Register \n(2) Login \n(3) Exit";
+
+        // Switch that uses a function which asks for input and checks if it's valid, if valid it returns the input.
+        switch(isNumber(1,3)){
             case 1:
-            // register
             registration();
             break;
-            
+
             case 2:
-            // login
             login();
             break;
-            
+
             case 3:
             // exit
             return 0;
-            
+
             default:
-            std::cout << "\"" << option << "\" is not a valid choice! Please try again.\n" << std::endl;
+                std::cout << "Critical Error Occurred! Please try again" << std::endl;
         }
     }
-    return 0;
+}
+
+// Function to check if input is numerical within the given range
+int isNumber(const int min, const int max){
+    int value;
+
+    while (true) {
+        // Get input from user
+        std::cout << "\nEnter choice:";
+        std::cin >> value;
+
+        // Check if numerical input was given
+        if (!std::cin || std::cin.peek() != '\n') {
+            // Clear fail state and empty cin
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Invalid input! Please enter a number.\n";
+            continue;
+        }
+        // Check if input is between correct min and max range
+        if (value < min || value > max) {
+            std::cout << "Error! Please choose between " << min << " and " << max << ".\n";
+            continue;
+        }
+
+        // Clear input
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        // If input passes all checks, return it.
+        return value;
+    }
 }
 
 void login(){
     bool loginSuccess = false;
     std::string username, password;
-    std::fstream readFile("data.txt");
-    
+    std::fstream readFile(fileName);
+
     // while loop to check for Successful login
     while (!loginSuccess){
         std::cout << "\nEnter your username and password";
@@ -61,7 +80,7 @@ void login(){
         std::cin >> username;
         std::cout << "Password:";
         std::cin >> password;
-        
+
         // Move reading pointer to start of file
         readFile.clear();
         readFile.seekp(0);
@@ -69,25 +88,29 @@ void login(){
 
         // While loop to go through data to check for username and password match
         while (std::getline(readFile, line)){
-            std::string tempUser, tempPass;
-            // sstream to allow data to be read as tokens 
+            // Temporary strings to store read data
+            std::string tempUser, tempPass, token, tempSecNum, tempSecAnswer;
+
+            // sstream to allow data to be read as tokens
             std::stringstream ss(line);
-            
+
             // Read each token up the comma and store it temporarily
             std::getline(ss, tempUser, ',');
             std::getline(ss, tempPass, ',');
+            std::getline(ss, token, ',');
+            std::getline(ss, tempSecAnswer, ',');
 
-            // Temp output for testing
-            std::cout << "\n\nUsername found: " << tempUser;
-            std::cout << "\nPassword found: " << tempPass;
-            
-            // Check if password and username match
+            // Convert string 'token' to integer
+            tempSecNum = std::stoi(token);
+
+            // Check if password and username match, if true close file and end outer loop
             if (username == tempUser && password == tempPass){
-                std::cout << "\nLogin success!" << std::endl;
+                std::cout << "\nLogin success!\n" << std::endl;
                 readFile.close();
                 loginSuccess = true;
-            } 
+            }
         }
+
         // Output if login wasn't successful
         if (!loginSuccess){
         std::cout << "\nInvalid username or password, please try again" << std::endl;
@@ -99,20 +122,30 @@ void registration() {
     std::string username;
     std::string password;
     std::string security_answer;
-    
-    std::cout << "\nRegistration chosen! Please enter your username and password." << std::endl;
+
+    std::cout << "\nRegistration chosen! Please enter your username and password. You will also be asked to enter a security question and answer." << std::endl;
     std::cout << "Username:";
     std::cin >> username;
     std::cout << "Password:";
     std::cin >> password;
-    
+
+    std::cout << "\nWhich security question would you like to use?";
+    std::cout << "\n1. What is your mother's maiden name?";
+    std::cout << "\n2. What was the name of your first pet?";
+    std::cout << "\n3. What was your childhood nickname?";
+    int security_question = isNumber(1,3);
+
+    std::cout << "\nInput security question answer (case sensitive):";
+    std::getline(std::cin, security_answer);
+
     // Check if data file opens
-    std::fstream outputFile("data.txt", std::ios::app);
+    std::fstream outputFile(fileName, std::ios::app);
     if (!outputFile.is_open()){
-        std::cout << "Error opening data file! Please try again";
+        std::cout << "Error opening data file!";
     }
+
     // Import data into file
-    outputFile << username << "," << password << "\n";
+    outputFile << username << "," << password << "," << security_question << "," << security_answer << "\n";
     outputFile.close();
     std::cout << "\nSuccessfully registered!" << std::endl;
 }
